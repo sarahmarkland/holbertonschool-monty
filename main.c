9da32a5271760a6ff4de6bdf9e17a3a45cc40b22
sarahmarkland <sarah.markland@icloud.com>
2023-03-31 15:29:34 -0500

    header, main updates

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "monty.h"
+
+FILE *fd = NULL;
 /**
  * main - entry function for Monty, the baby python
  * @argc: number of command line args passed
@@ -7,43 +9,107 @@
  */
 int main(int argc, char **argv)
 {
+	void (*f)(stack_t **, unsigned int) = NULL;
+	char *buffer = NULL, *token = NULL, *op = NULL, *n = NULL;
+	size_t length = 0;
 	stack_t *stack = NULL;
-	unsigned int line_number = 0;
-	FILE *file = NULL;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read = 0;
-	instruction_t opcodes[] = {
+	const char deliminators[] = " \t\n";
+	unsigned int line_number = 1;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&buffer, &length, fd) != -1)
+	{
+		token = strtok(buffer, deliminators);
+		strcpy(op, token);
+		f = get_func(&stack, line_number, token);
+		if(strcmp(op, "push") == 0)
+		{
+			token = strtok(NULL, deliminators);
+			if (token == NULL)
+			{
+				free(buffer), buffer = NULL, free_stack(&stack);
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			}
+			strcpy(n, token);
+		}
+		free(buffer), buffer = NULL, f(&stack, line_number);
+		if (strcmp(op, "push") == 0)
+			set_n(&stack, line_number, n);
+	}
+	free(buffer), buffer = NULL, free_stack(&stack);
+	return (EXIT_SUCCESS);
+}
+/**
+ * get_func - calls function matched to opcode
+ * @stack: linked list
+ * @l: line number
+ * @code: opcode
+ * Return: pointer to matched function
+ */
+void (*get_func(stack_t **stack, int l, char *code))(stack_t **, unsigned int)
+{
+	int i = 0;
+	instruction_t instructions[] = {
 		{"push", push},
 		{"pall", pall},
 		{"pint", pint},
 		{"pop", pop},
 		{"swap", swap},
 		{"add", add},
-		{"nop", nop},
-		{NULL, NULL}
+		{"nop", nop}
 	};
-	if (argc != 2)
+
+	while (strcmp(code, instructions[i].opcode) != 0)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
+		i++;
+		if (i > 7)
+		{
+			fprintf(stderr, "L%d: unknown instructions %s\n", l, code);
+			free_stack(stack);
+			close_error();
+		}
 	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
+	return (instructions[i].f);
+}
+
+/**
+ * set_n - sets the value of n
+ * @stack: linked list
+ * @line_num: current line number
+ * @num: number to set n to
+ */
+void set_n(stack_t **stack, unsigned int line_num, char *num)
+{
+	if (strcmp(num, "0") == 0)
+		(*stack)->n = 0;
+	if (strcmp(num, "0") != 0)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		(*stack)->n = atoi(num);
+		if ((*stack)->n == 0 || (num[0] != '-' && (*stack)->n == 1))
+		{
+			fprintf(stderr, "Error: L%d: usage: push integer\n", line_num);
+			free_stack(stack);
+			close_error();
+		}
 	}
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		line_number++;
-		tokentime(line, " \n\t", &stack);
-		op_fun_res(&stack, opcodes);
-	}
-	{
-		free(line);
-		fclose(file);
-		free_stack(&stack, NULL);
-	}
-	return (0);
+}
+
+/**
+ * close_error - closes file and exits with failure
+ * Return: void
+ */
+void close_error(void)
+{
+	fclose(fd);
+	exit(EXIT_FAILURE);
 }
